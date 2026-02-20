007ee3a16ab482fbb1c1cbdf4a303794951296be
Mira van Gemmeren <mira.vangemmeren@ru.nl>
2026-02-20 14:44:47 +0100

    Added PyRegistry.

--- crates/ci-python/src/lib.rs
+++ crates/ci-python/src/lib.rs
@@ -1,1 +1,11 @@
+use pyo3::prelude::*;
 
+mod pyregistry;
+
+pub use crate::pyregistry::PyRegistry;
+
+#[pymodule]
+fn ci_python(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
+    m.add_class::<PyRegistry>()?;
+    Ok(())
+}
