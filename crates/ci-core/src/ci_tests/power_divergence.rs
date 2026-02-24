6693333d91aa3064f2f2ef90859bccb53a0677df
Hessel Monsma <hessel.monsma@ru.nl>
2026-02-24 15:10:20 +0100

    fixed formatting

--- crates/ci-core/src/ci_tests/power_divergence.rs
+++ crates/ci-core/src/ci_tests/power_divergence.rs
@@ -4,11 +4,11 @@ pub struct PowerDivergence {
     // Object traits
 }
 
-fn bincount(v: Vec<i32>, minlength: usize) -> Vec<i32>{
+fn bincount(v: Vec<i32>, minlength: usize) -> Vec<i32> {
     let mut result: Vec<i32> = vec![0; minlength];
-    for i in v{
-        if result.len() < (i as usize) + 1{
-            result.resize((i as usize)+1, 0);
+    for i in v {
+        if result.len() < (i as usize) + 1 {
+            result.resize((i as usize) + 1, 0);
         }
         result[i as usize] += 1;
     }
