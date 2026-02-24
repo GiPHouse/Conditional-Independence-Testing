a745ba395a15c79e8c20fa6813c352ab79543b36
Hessel Monsma <hessel.monsma@ru.nl>
2026-02-24 15:07:06 +0100

    added bincount function

--- crates/ci-core/src/ci_tests/power_divergence.rs
+++ crates/ci-core/src/ci_tests/power_divergence.rs
@@ -4,6 +4,17 @@ pub struct PowerDivergence {
     // Object traits
 }
 
+fn bincount(v: Vec<i32>, minlength: usize) -> Vec<i32>{
+    let mut result: Vec<i32> = vec![0; minlength];
+    for i in v{
+        if result.len() < (i as usize) + 1{
+            result.resize((i as usize)+1, 0);
+        }
+        result[i as usize] += 1;
+    }
+    result
+}
+
 impl CITest for PowerDivergence {
     fn run_test(&self) {}
     //Other necessary stuff
