3cf197b7fdf569b3707c6712d843528d4f60411a
Hessel Monsma <hessel.monsma@ru.nl>
2026-03-05 13:42:20 +0100

    Changed contingency_table to work on DataFrames instead of ndarray

--- crates/ci-core/src/ci_tests/power_divergence.rs
+++ crates/ci-core/src/ci_tests/power_divergence.rs
@@ -1,6 +1,6 @@
+use polars::prelude::*;
 use scirs2_core::prelude::*;
 use std::{collections::HashMap, usize};
-use core::hash::Hash;
 
 use crate::strategy::CITest;
 
@@ -8,31 +8,38 @@ pub struct PowerDivergence {
     // Object traits
 }
 
-fn contingency_table<Q>(data: &Array2<Q>, col1: usize, col2: usize) -> Array2<usize> where Q: Eq + Hash {
-    let rows = data.len_of(Axis(0));
-    let mut col1_data_map: HashMap<&Q, usize> = HashMap::new(); 
+fn contingency_table(data: &DataFrame, col1: &String, col2: &String) -> Array2<usize> {
+    let column1 = data.column(col1).unwrap().as_materialized_series();
+    let column2 = data.column(col2).unwrap().as_materialized_series();
+    let mut col1_data_map = HashMap::new(); 
     let mut col1_size: usize = 0;
-    let mut col2_data_map: HashMap<&Q, usize> = HashMap::new();
-    let mut col2_size: usize = 0;
-    for row in 0..rows {
-        if !col1_data_map.contains_key(&data[[row,col1]]) {
-            col1_data_map.insert(&data[[row,col1]], col1_size);
+    for i in column1.iter() {
+        if !col1_data_map.contains_key(&i) {
+            col1_data_map.insert(i, col1_size);
             col1_size += 1;
         }
-        if !col2_data_map.contains_key(&data[[row,col2]]) {
-            col2_data_map.insert(&data[[row,col2]], col2_size);
+    }
+    let mut col2_data_map = HashMap::new();
+    let mut col2_size: usize = 0;
+    for i in column2.iter() {
+        if !col2_data_map.contains_key(&i) {
+            col2_data_map.insert(i, col2_size);
             col2_size += 1;
         }
     }
     let mut result = Array::zeros((col1_size,col2_size));
-    for row in 0..rows {
-        let i = &data[[row,col1]];
-        let j = &data[[row,col2]];
-        if let Some(result_row) = col1_data_map.get(i) {
-            if let Some(result_col) = col2_data_map.get(j) {
+    let mut it1 = column1.iter();
+    let mut it2 = column2.iter();
+    let mut i = it1.next();
+    let mut j = it2.next();
+    while i.is_some() && j.is_some()  {
+        if let Some(result_row) = col1_data_map.get(&i.unwrap()) {
+            if let Some(result_col) = col2_data_map.get(&j.unwrap()) {
                 result[[*result_row, *result_col]] += 1;
             }
         }
+        i = it1.next();
+        j = it2.next();
     }
     result
 }
