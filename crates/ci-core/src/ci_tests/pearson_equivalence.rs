2c86387c026ab463cfbea0db0f659b5e2d904d43
Hessel Monsma <hessel.monsma@ru.nl>
2026-04-14 16:32:29 +0200

    naive untested first version of pearson_equivalence based on pgmpy

--- crates/ci-core/src/ci_tests/pearson_equivalence.rs
+++ crates/ci-core/src/ci_tests/pearson_equivalence.rs
@@ -1,5 +1,8 @@
 use crate::strategy::{CITest, CITestDataType, TestResult};
-use ndarray::{Array1, Array2};
+use crate::ci_tests::pearson_correlation::{PearsonCorrelation, wrap_result};
+use ndarray::{Array1, Array2, Axis};
+use libm::{atanh, sqrt};
+use statrs::distribution::{ContinuousCDF, Normal};
 
 #[allow(dead_code)]
 pub struct PearsonEquivalence {}
@@ -7,13 +10,51 @@ pub struct PearsonEquivalence {}
 impl CITest for PearsonEquivalence {
     fn run_test(
         &self,
-        _x_values: Array1<f64>,
-        _y_values: Array1<f64>,
-        _z: Array2<f64>,
-        _boolean: bool,
-        _significance_level: f64,
+        x_values: Array1<f64>,
+        y_values: Array1<f64>,
+        z: Array2<f64>,
+        boolean: bool,
+        significance_level: f64,
     ) -> anyhow::Result<TestResult> {
-        todo!()
+        // placeholder, need to ask about hyperparameters
+        let delta_treshold = 0.1;
+
+        let n = x_values.len() as f64;
+        let s = z.axis_iter(Axis(1)).len() as f64;
+
+        let pearsonr = PearsonCorrelation{}.run_test(x_values, y_values, z, false, significance_level);
+        let statistic = match pearsonr {
+            Ok(TestResult::PValue(_, statistic)) => statistic,
+            Ok(_) => 0.,
+            Err(e) => return Err(e)
+        };
+        let rho = if statistic <= -1. { 
+            -0.9999999 
+        } else {
+            if statistic >= 1. {
+                0.99999999 
+            } 
+            else {statistic}
+        };
+
+        let coefficient = atanh(rho);
+        let z_delta = atanh(delta_treshold);
+
+        let std_error_factor = sqrt(n - s - 3.);
+
+        let z_score_lower = std_error_factor * (coefficient + z_delta);
+        let p_value_lower = 1. - Normal::new(0.0, 0.1).unwrap().cdf(z_score_lower);
+
+        let z_score_upper = std_error_factor * (coefficient - z_delta);
+        let p_value_upper = 1. - Normal::new(0.0, 0.1).unwrap().cdf(z_score_upper);
+
+        let p_value = if p_value_lower > p_value_upper { 
+            p_value_lower 
+        } else {
+            p_value_upper
+        };
+        
+        Ok(wrap_result(boolean, p_value, coefficient, significance_level))
     }
 
     fn data_types(&self) -> &'static [CITestDataType] {
