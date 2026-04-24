602c1f85996cde7bd8427e15e3c8d31a05932c3e
Hiddentale <55276630+Hiddentale@users.noreply.github.com>
2026-04-24 14:45:34 +0200

    feat: working basic r bindings

---
+++ crates/cir/tests/testthat/test-chi_squared.R
@@ -0,0 +1,34 @@
+library(cir)
+
+test = RChiSquared$new(FALSE, 0.05)
+
+test_that("independent data is not rejected", {
+    x  = c(1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 2.0, 2.0)
+    y = c(1.0, 2.0, 1.0, 2.0, 1.0, 2.0, 1.0, 2.0)
+    z = matrix(0, nrow=8, ncol=0)
+    
+    result = test$run_test(x, y, z)
+    expect_equal(result$kind, "statistic")
+    expect_true(result$statistic < 1e-9)
+    expect_true(result$p_value >= 0.99)
+    expect_equal(result$df, 1)
+    test = RChiSquared$new(TRUE, 0.05)
+    result = test$run_test(x, y, z)
+    expect_true(result$independent)
+})
+
+test_that("dependent data is rejected", {
+    x = c(1., 1., 1., 1., 2., 2., 2., 2.)
+    y = c(1., 1., 1., 1., 2., 2., 2., 2.)
+    z = matrix(0, nrow=8, ncol=0)
+
+    result = test$run_test(x, y, z)
+    expect_equal(result$kind, "statistic")
+    expect_true(abs(result$statistic - 8.0) < 1e-9)
+    expect_true(abs(result$p_value - 0.004677734981047276) < 1e-12)
+    expect_equal(result$df, 1)
+    test = RChiSquared$new(TRUE, 0.05)
+    result = test$run_test(x, y, z)
+    expect_false(result$independent)
+})
+
