94a44a7dfa55a6ea7d38fbbe7399c53f5cbfbf0a
silkefaas <146170943+silkefaas@users.noreply.github.com>
2026-05-22 15:58:25 +0200

    contingency_test, contingency_table and power_divergence optimization (#150)
    * added textfile for draft pull request
    
    * removed unneccessary behaviour within global category map, since the contingency test doesnt care about the layout of the contingency matrix as long as the values are the same
    
    * potential optimization of contingency_test
    
    * benchmark for contingency test
    
    * changed logic within G-test to get rid of division
    
    * trying to get rid of pull error
    
    * more intensive benchmark
    
    * optimised contingency_test code
    
    * final version of optimised contingecy_test
    
    * power divergence benchmark function
    
    * optimization of contingency_table and power_divergence and also added benchmarks
    
    * fixed errors that made log_likelihood and modified_likelihood fail (+ and - were used incorrectly)
    
    * Final optimization of contingency_test
    
    * forgot to delete a file
    
    * also added the optimized versions of contingency_table and power_divergence and the benchmarks used
    
    * comment in contingency_test made more clear
    
    * case distinction for Freeman Tukey added including two unit tests
    
    * broke down contingency_test function in smaller helper functions
    
    * some comments were not great yet
    
    ---------
    
    Co-authored-by: t3hinator <marcel.lemay.enschede@gmail.com>

--- crates/ci-core/src/utils/power_divergence.rs
+++ crates/ci-core/src/utils/power_divergence.rs
@@ -1,6 +1,6 @@
 use crate::strategy::TestResult;
 use crate::utils::contingency_table::{
-    build_global_category_map, contingency_table, contingency_table_with_categories,
+    build_global_category_map, contingency_table, contingency_table_from_indices,
 };
 use crate::utils::contingency_test::contingency_test;
 use crate::utils::partition_indices::partition_indices;
@@ -37,10 +37,15 @@ pub fn power_divergence(
 
     let mut statistic = 0.0;
     let mut degrees_of_freedom = 0;
+
     for indices in partition_indices(z) {
-        let x_sub: Array1<f64> = indices.iter().map(|&i| x_values[i]).collect();
-        let y_sub: Array1<f64> = indices.iter().map(|&i| y_values[i]).collect();
-        let table = contingency_table_with_categories(&x_sub, &y_sub, &x_categories, &y_categories);
+        let table = contingency_table_from_indices(
+            &indices,
+            x_values,
+            y_values,
+            &x_categories,
+            &y_categories,
+        );
         let Ok((stat, _p, dof)) = contingency_test(&table, lambda) else {
             continue;
         };
