3136899fe92b7c601ed656a82ef2bbf1ed996b78
Mira van Gemmeren <mira.vangemmeren@ru.nl>
2026-05-27 13:23:08 +0200

    (ci-python) Removed commented out example binding from lib.rs.

--- crates/ci-python/src/lib.rs
+++ crates/ci-python/src/lib.rs
@@ -19,58 +19,6 @@ mod _ci_python {
 
     include!(concat!(env!("OUT_DIR"), "/ci_tests.rs"));
 
-    // #[gen_stub_pyclass]
-    // #[pyclass]
-    // struct PyPearsonCorrelation {
-    //     inner: ci_core::ci_tests::PearsonCorrelation,
-    // }
-
-    // #[gen_stub_pymethods]
-    // #[pymethods]
-    // impl PyPearsonCorrelation {
-    //     #[new]
-    //     fn new(significance_level: f64) -> Self {
-    //         Self {
-    //             inner: ci_core::ci_tests::PearsonCorrelation {
-    //                 boolean: true,
-    //                 significance_level: significance_level,
-    //             },
-    //         }
-    //     }
-
-    //     #[getter]
-    //     fn significance_level(&self) -> PyResult<f64> {
-    //         Ok(self.inner.significance_level)
-    //     }
-
-    //     #[setter]
-    //     fn set_significance_level(&mut self, significance_level: f64) -> PyResult<()> {
-    //         self.inner.significance_level = significance_level;
-    //         Ok(())
-    //     }
-
-    //     fn run_test(
-    //         &self,
-    //         py: Python<'_>
-    //         x_values: PyReadonlyArray1<'_, f64>,
-    //         y_values: PyReadonlyArray1<'_, f64>,
-    //         z: PyReadonlyArray2<'_, f64>,
-    //     ) -> PyResult<Py<PyAny>> {
-    //         test_result_to_pyobj(
-    //             &self.inner
-    //                 .run_test(
-    //                     x_values.as_array().to_owned(),
-    //                     y_values.as_array().to_owned(),
-    //                     z.as_array().to_owned(),
-    //                 )
-    //                 .map_err(|e| {
-    //                     PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string())
-    //                 })?,
-    //             py
-    //         )
-    //     }
-    // }
-
     #[pymodule_init]
     fn init(m: &Bound<'_, PyModule>) -> PyResult<()> {
         ci_tests_init::init(m)
