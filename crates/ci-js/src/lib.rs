347f0600d19b289cf04b1003a2aa6afc73344ae7
Oliver Baron <247354883+ol590@users.noreply.github.com>
2026-06-03 10:52:58 +0200

    Moved JS type conversion to seperate file

--- crates/ci-js/src/lib.rs
+++ crates/ci-js/src/lib.rs
@@ -3,10 +3,10 @@ use ci_core::ci_tests::{
     log_likelihood::LogLikelihood, modified_likelihood::ModifiedLikelihood,
     pearson_correlation::PearsonCorrelation, pearson_equivalence::PearsonEquivalence,
 };
-use ci_core::strategy::{CITest, TestResult};
-use js_sys::{Array, Float64Array};
-use ndarray::{Array1, Array2};
+use ci_core::strategy::CITest;
+use js_sys::Float64Array;
 use wasm_bindgen::prelude::*;
+mod util;
 
 #[wasm_bindgen]
 pub fn init() {
@@ -33,12 +33,12 @@ macro_rules! wasm_ci_test {
             boolean: bool,
             significance_level: f64,
         ) -> Result<JsValue, JsValue> {
-            let (x, y, z) = convert_to_ndarray(z_flat, x, y, z_rows, z_cols)?;
+            let (x, y, z) = util::convert_to_ndarray(z_flat, x, y, z_rows, z_cols)?;
             let test = <$inner>::new(boolean, significance_level);
             let result = test
                 .run_test(x, y, z)
                 .map_err(|e| JsValue::from_str(&e.to_string()))?;
-            convert_to_jsvalue(&result)
+            util::convert_to_jsvalue(&result)
         }
     };
 }
@@ -74,58 +74,12 @@ pub fn pearson_equivalence_test(
     delta_threshold: f64,
 ) -> Result<JsValue, JsValue> {
     // Convert JavaScript Float64Array -> Vec<f64> -> ndarray:Array
-    let (x, y, z) = convert_to_ndarray(z_flat, x, y, z_rows, z_cols)?;
+    let (x, y, z) = util::convert_to_ndarray(z_flat, x, y, z_rows, z_cols)?;
     let citest = PearsonEquivalence::new(boolean, significance_level, delta_threshold);
 
     let result = citest
         .run_test(x, y, z)
         .map_err(|e| JsError::new(&e.to_string()))?;
 
-    convert_to_jsvalue(&result)
-}
-
-#[allow(clippy::type_complexity)]
-fn convert_to_ndarray(
-    z_flat: &Float64Array,
-    x: &Float64Array,
-    y: &Float64Array,
-    z_rows: usize,
-    z_cols: usize,
-) -> Result<(Array1<f64>, Array1<f64>, Array2<f64>), JsValue> {
-    let z_vec: Vec<f64> = z_flat.to_vec();
-    let x_vec: Vec<f64> = x.to_vec();
-    let y_vec: Vec<f64> = y.to_vec();
-
-    let z: Array2<f64> = if z_vec.is_empty() {
-        Array2::zeros((x_vec.len(), 0)) // correct empty shape
-    } else {
-        Array2::from_shape_vec((z_rows, z_cols), z_vec)
-            .map_err(|e| JsValue::from_str(&e.to_string()))?
-    };
-    let x: Array1<f64> = Array1::from_vec(x_vec);
-    let y: Array1<f64> = Array1::from_vec(y_vec);
-
-    Ok((x, y, z))
-}
-
-#[allow(clippy::cast_precision_loss)]
-#[allow(clippy::unnecessary_wraps)]
-fn convert_to_jsvalue(result: &TestResult) -> Result<JsValue, JsValue> {
-    match result {
-        TestResult::Boolean(b) => Ok(JsValue::from_bool(*b)),
-        TestResult::PValue(p_value, coefficient) => {
-            let array = Array::new();
-            array.push(&JsValue::from_f64(*p_value));
-            array.push(&JsValue::from_f64(*coefficient));
-            Ok(array.into())
-        }
-
-        TestResult::Statistic(p_value, statistic, dof) => {
-            let array = Array::new();
-            array.push(&JsValue::from_f64(*p_value));
-            array.push(&JsValue::from_f64(*statistic));
-            array.push(&JsValue::from_f64(*dof as f64));
-            Ok(array.into())
-        }
-    }
+    util::convert_to_jsvalue(&result)
 }
