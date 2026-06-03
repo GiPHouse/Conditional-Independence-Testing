8af8dce27f22cda6fd1a8c5b93af8ba806b52e3a
Hessel Monsma <hessel.monsma@ru.nl>
2026-06-03 12:03:04 +0200

    Made modified log likelihood behave as intended

--- crates/ci-core/src/ci_tests/modified_likelihood.rs
+++ crates/ci-core/src/ci_tests/modified_likelihood.rs
@@ -162,10 +162,7 @@ mod tests {
         let x = array![1., 1., 2., 2., 1., 1., 2., 2.];
         let y = array![1., 1., 2., 2., 1., 1., 2., 2.];
         let z = array![[1.], [1.], [1.], [1.], [2.], [2.], [2.], [2.]];
-        let r = t.run_test(x, y, z);
-        assert!(
-            r.is_err(),
-            "Modified Likelihood should error out independent data"
-        );
+        let r = t.run_test(x, y, z).unwrap();
+        assert!(matches!(r, TestResult::Boolean(false)));
     }
 }
