480b0c51de34d3329651b2d7f5721c24da38800b
silkefaas <146170943+silkefaas@users.noreply.github.com>
2026-06-03 15:37:50 +0200

    bug fix in error handling of power divergence (#232)
    * added textfile for draft pull request
    
    * removed unneccessary behaviour within global category map, since the contingency test doesnt care about the layout of the contingency matrix as long as the values are the same
    
    * potential optimization of contingency_test
    
    * benchmark for contingency test
    
    * changed logic within G-test to get rid of division
    
    * trying to get rid of pull error
    
    * more intensive benchmark
    
    * optimised contingency_test code
    
    * final version of optimised contingecy_test
    
    * power divergence benchmark function
    
    * optimization of contingency_table and power_divergence and also added benchmarks
    
    * fixed errors that made log_likelihood and modified_likelihood fail (+ and - were used incorrectly)
    
    * Final optimization of contingency_test
    
    * forgot to delete a file
    
    * also added the optimized versions of contingency_table and power_divergence and the benchmarks used
    
    * comment in contingency_test made more clear
    
    * case distinction for Freeman Tukey added including two unit tests
    
    * broke down contingency_test function in smaller helper functions
    
    * some comments were not great yet
    
    * minor pearson_equivalence fix
    
    * optimization of pearson_correlation and the corresponding benchmark
    
    * fixed failing formatter in pearson_equivalence
    
    * deleted benchmarks
    
    * changed some parts according to comments from the pull request
    
    * removed an unnecessary comment
    
    * fix of bug in error handling of power divergence and fixing unit test modified likelihood
    
    * removed left over code and modified test to work with err instead of boolean
    
    * oopsiedaisy forgot to run the checklist
    
    * Made modified log likelihood behave as intended
    
    * Changed p_value check in unit test
    
    * fixed accidental formatting error
    
    ---------
    
    Co-authored-by: t3hinator <marcel.lemay.enschede@gmail.com>
    Co-authored-by: Hessel Monsma <hessel.monsma@ru.nl>

--- crates/ci-core/src/ci_tests/modified_likelihood.rs
+++ crates/ci-core/src/ci_tests/modified_likelihood.rs
@@ -163,6 +163,6 @@ mod tests {
         let y = array![1., 1., 2., 2., 1., 1., 2., 2.];
         let z = array![[1.], [1.], [1.], [1.], [2.], [2.], [2.], [2.]];
         let r = t.run_test(x, y, z).unwrap();
-        assert!(matches!(r, TestResult::Boolean(true)));
+        assert!(matches!(r, TestResult::Boolean(false)));
     }
 }
